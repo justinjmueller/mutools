4fa6f66d96c2aa91c94745f82f25d9cf22c8e004
Justin Mueller <mueller@fnal.gov>
2026-01-25 15:25:00 -0600

    Refactor mark_axis function for improved code style and readability

--- src/mutools/plotting/helpers.py
+++ src/mutools/plotting/helpers.py
@@ -1,12 +1,12 @@
-import matplotlib.pyplot as plt
 from typing import Optional
+
 
 def mark_axis(
     ax,
-    label : str,
-    vadj : Optional[float] = 0,
-    hadj : Optional[float] = 0,
-    alignment : Optional[str] = 'left'
+    label: str,
+    vadj: Optional[float] = 0,
+    hadj: Optional[float] = 0,
+    alignment: Optional[str] = "left",
 ) -> None:
     """
     Mark the axis with a contextual label. The adjustment parameters
@@ -26,11 +26,11 @@ def mark_axis(
         Horizontal adjustment for the label position.
     """
     yrange = ax.get_ylim()
-    usey = yrange[1] + 0.01*(yrange[1] - yrange[0]) + vadj*(yrange[1] - yrange[0])
+    usey = yrange[1] + 0.01 * (yrange[1] - yrange[0]) + vadj * (yrange[1] - yrange[0])
     xrange = ax.get_xlim()
-    usex = xrange[0] + 0.025*(xrange[1] - xrange[0]) + hadj*(xrange[1] - xrange[0])
-    if alignment == 'left':
-        ax.text(x=usex, y=usey, s=label, fontsize=14, color='#d67a11')
-    elif alignment == 'right':
-        usex = xrange[1] - 0.025*(xrange[1] - xrange[0]) + hadj*(xrange[1] - xrange[0])
-        ax.text(x=usex, y=usey, s=label, fontsize=14, color='#d67a11', horizontalalignment='right')
+    usex = xrange[0] + 0.025 * (xrange[1] - xrange[0]) + hadj * (xrange[1] - xrange[0])
+    if alignment == "left":
+        ax.text(x=usex, y=usey, s=label, fontsize=14, color="#d67a11")
+    elif alignment == "right":
+        usex = xrange[1] - 0.025 * (xrange[1] - xrange[0]) + hadj * (xrange[1] - xrange[0])
+        ax.text(x=usex, y=usey, s=label, fontsize=14, color="#d67a11", horizontalalignment="right")
